056e5ad4fd346faf0fe980810912457670d8003c
dltorrise <dltorrise@gmail.com>
2023-02-20 22:06:11 -0600

    got delete to work

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,6 @@
 const notes = require('express').Router();
 const path = require('path');
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-//const uuid = require('../helpers/uuid')
+const { writeToFile, readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const { uuid } = require('uuidv4');
 
 // GET Route for retrieving all the notes
@@ -28,15 +27,29 @@ notes.post('/', (req, res) => {
   }
 });
 
-// notes.delete('/:id', (req, res) => {
-//     readFromFile(path.join(__dirname, '../db/db.json')).then((data) => 
-//     res.json(JSON.parse(data))).then(newData)
-//     if(':id'===newData.id) {
-//         res.send(newData)
-//         res.json('Note deleted')
-//     } else {
-//         res.error('Something went wrong.')
-//     }
-// })
+
+//BONUS
+//Delete Route
+notes.delete('/:id', (req, res) => {
+  const {id} = req.params
+  readFromFile(path.join(__dirname, '../db/db.json')).then((data) => {
+    //creates a variable to store array
+    const parsedData = JSON.parse(data)
+    //console.log(parsedData)
+    for (i=0; i<parsedData.length; i++) {
+      if (parsedData[i].id===id) {
+        //creates a new array with all elements except for element that matches id
+        const newArray = parsedData.splice(!i)
+        //writes new array to file
+        writeToFile(path.join(__dirname, '../db/db.json'), newArray)
+        //renders notes without deleted element
+        readFromFile(path.join(__dirname, '../db/db.json')).then((data) => res.json(JSON.parse(data))); 
+        console.log("Message deleted")
+      } else {
+        console.log("Message not deleted")
+      }
+    }
+  })
+})
 
 module.exports = notes;
